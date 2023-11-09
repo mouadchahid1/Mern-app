1aaa407e7c905ba8dff81d5df4d7a9260fdb59c9
mouad chahid <mouadechahide1@gmail.com>
2023-11-09 15:09:00 +0100

    add file .env and start jwt for auth

--- server/index.js
+++ server/index.js
@@ -2,18 +2,19 @@ import express from "express"
 import bodyParser from "body-parser" ;
 import cors from "cors" ; 
 import mongoose from "mongoose"  ; 
-import PostRouter from "./routes/posts.js";
+import PostRouter from "./routes/posts.js"; 
+import dotenv from "dotenv"; 
 
-const app = express() ;   
+const app = express() ;  
+dotenv.config() ;
 
 app.use(bodyParser.json({limit:"30mb" , extended : true})) ;   
 app.use(bodyParser.urlencoded({limit : "30mb", extended : true}));
 app.use(cors()) ;
 
 app.use("/posts",PostRouter) ;
-const CONNECTION_URL = "mongodb+srv://mouadchahid05:mouad05@hinditp.0s2fpgo.mongodb.net/?retryWrites=true&w=majority" ;  
 const PORT = process.env.PORT || 5000 ;
-mongoose.connect(CONNECTION_URL,{useNewUrlParser : true , useUnifiedTopology : true }) 
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser : true , useUnifiedTopology : true }) 
 .then(() =>app.listen(PORT,()=> {console.log(`app is listen on ${PORT}`)})) 
 .catch((error)=>{console.log(error)}); 
 
